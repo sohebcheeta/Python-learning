7627e8d11e89202c532ef8750decf9d98ea39e60
Soheb <sohebcheeta007@gmail.com>
2026-09-16 10:37:02 +0530

    Add Day 7 conditions practice

--- day7_conditions.py
+++ day7_conditions.py
@@ -1,1 +1,14 @@
+numbers = [4, 9, 12, 17, 21, 25, 30, 11]
+for number in numbers:
+if number % 3 == 0:
+print(number)
 
+numbers = [5, 12, 18, 23, 30, 41, 48, 55]
+for number in numbers:
+if number > 20 and number %3 == 0:
+print(number)
+
+numbers = [7, 10, 15, 22, 28, 33, 40, 45]
+for number in numbers:
+if (number > 20 and number % 2 == 0) or number == 15:
+print(number)
